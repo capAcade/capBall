f4203d4e439bb88bfeb987f585bdc417094bdc48
Juan Besselse <C45506@launcher.int.abnamro.com>
2018-12-03 18:25:03 +0100

    replaced capball

--- README.md
+++
@@ -1,2 +0,0 @@
-# capBall
-pinball game
--- src/game.js
+++ src/game.js
@@ -1,190 +1,271 @@
-var game = new Phaser.Game(1280, 1024, Phaser.CANVAS, 'phaser-example', { create: create, update: update, render: render });
+import Boundary from './boundary.js'
+import Paddle from './paddle.js'
+import Stopper from './stopper.js'
+import Config from './config.js'
+import Launcher from './launcher.js';
+// plugins
+Matter.use(MatterAttractors);
 
-var outlineVertices = [1440,-3186.59,1376.96,-3195.95,1023.88,-2194.34,1345.45,-1961.25,1345.45,-663.375,
-    638.684,-480.341,160.054,-154.361,150.206,471.008,-318.575,470.023,-319.559,-153.376,
-    -800.158,-480.341,-1519.35,-619.21,-1518.23,-1988.98,-1147.38,-2175.73,-1429.31,-3152.17,
-    -1500.1,-3195.52,-1492.51,-3399.68,-1438.82,-3867.33,-1309.07,-4132.41,-1112.22,-4351.64,
-    -787.851,-4540.67,-389.666,-4670.41,139.843,-4778.28,655.92,-4846.15,872.006,-4837.87,
-    1067.14,-4792.27,1236.26,-4700.02,1374.63,-4584.71,1480.34,-4440.57,1557.21,-4271.45,
-    1601.65,-3992.59,1601.41,-3712.19,1604.11,-2197.86,1814.72,-2234.37,1848.77,-2057.55,
-    1600,-2003.04,1600,-171.408,1442.93,-169.434,1439.5,-663.941];
+// constants
 
-var launcherVertices = [1401.35,-224.963,1631.98,-224.963];
-var guide1Vertices = [-825.819,-746.541,-771.419,-853.487,-1280,-1120,-1280,-1759.99,-1360,-1759.99,
-    -1360,-959.993];
-var guide2Vertices = [663.001,-743.13,614.862,-855.893,1119.91,-1121.82,1123.3,-1760.68,1200.08,-1759.99,
-    1200.08,-959.993];
-var guide3Vertices = [-1116.81,-1753.55,-1118.98,-1277.67,-878.975,-1117.67];
-var guide4Vertices = [721.698,-1128.55,956.731,-1282.32,956.731,-1762.32];
-var gutterVertices = [-480.857,413.599,293.837,413.599];
+// score elements
+let $currentScore = $('.current-score span');
+let $highScore = $('.high-score span');
 
-var bouncer1 = [-1042.47,-1763.21,-826.297,-1160.74];
-var bouncer2 = [883.433,-1767.01,669.858,-1171.73];
-
-var smallCircles = [1406,-3183.89,-1320,-1759.99,-1078.86,-1753.03,-861.88,-1152.05,918.723,-1760.8,1160.08,-1759.99,704.87,-1161.38];
-var mediumCircles = [-1500.82,-3132.63,-866.447,-3163.29,-290.006,-3074.23,187.945,-3415.55,614.227,-3074.23,-451.581,-2232.34,396.293,-2242];
-var largeCircles = [-446.686,-3704.69,309.841,-4133.62,995.658,-3595.05];
-
-var leftFlipperVertices = [560,32,560,-32,0,-64,0,64];
-var rightFlipperVertices = [0,64,0,-64,-560,-32,-560,32];
-
-var ballStart = [17.5016, -21.318];
-
-var arrayMultiplier = 1.5;
-
-multiplyArray = function(array, multiplier) {
-    var i;
-    var newArray = [];
-    for (i=0; i < array.length; i++) { 
-        newArray[i] = array[i] * multiplier;
-    }
-    return newArray;
-};
-
-outlineVertices = multiplyArray(outlineVertices, arrayMultiplier);
-launcherVertices = multiplyArray(launcherVertices, arrayMultiplier);
-guide1Vertices = multiplyArray(guide1Vertices, arrayMultiplier);
-guide2Vertices = multiplyArray(guide2Vertices, arrayMultiplier);
-guide3Vertices = multiplyArray(guide3Vertices, arrayMultiplier);
-guide4Vertices = multiplyArray(guide4Vertices, arrayMultiplier);
-gutterVertices = multiplyArray(gutterVertices, arrayMultiplier);
-bouncer1 = multiplyArray(bouncer1, arrayMultiplier);
-bouncer2 = multiplyArray(bouncer2, arrayMultiplier);
-smallCircles = multiplyArray(smallCircles, arrayMultiplier);
-mediumCircles = multiplyArray(mediumCircles, arrayMultiplier);
-largeCircles = multiplyArray(largeCircles, arrayMultiplier);
-leftFlipperVertices = multiplyArray(leftFlipperVertices, arrayMultiplier);
-rightFlipperVertices = multiplyArray(rightFlipperVertices, arrayMultiplier);
-ballStart = multiplyArray(ballStart, arrayMultiplier);
-
-var ballBody;
-var flipperJoints = [];
-var PTM = 100 * arrayMultiplier; // conversion ratio for values in arrays above
-var needReset = false;
-
-function create() {
-    
-    game.world.setBounds(-400 * arrayMultiplier, -520 * arrayMultiplier, 800 * arrayMultiplier, 600 * arrayMultiplier);
-    
-    game.stage.backgroundColor = '#0070AD';
-
-    // Enable Box2D physics
-    game.physics.startSystem(Phaser.Physics.BOX2D);
-    game.physics.box2d.ptmRatio = 500;
-    game.physics.box2d.gravity.y = 5000; // large gravity to make scene feel smaller
-    game.physics.box2d.friction = 0.1;
-    
-    // Make the ground body
-    var mainBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0);
-    
-    // Add bounce-less fixtures
-    game.physics.box2d.restitution = 0.1;
-    mainBody.addLoop(outlineVertices);
-    mainBody.addLoop(guide1Vertices);
-    mainBody.addLoop(guide2Vertices);
-    mainBody.addChain(guide3Vertices);
-    mainBody.addChain(guide4Vertices);
-    
-    // Add bouncy fixtures
-    game.physics.box2d.restitution = 1;
-    mainBody.addEdge(bouncer1[0], bouncer1[1], bouncer1[2], bouncer1[3]);
-    mainBody.addEdge(bouncer2[0], bouncer2[1], bouncer2[2], bouncer2[3]);
-
-    for (var i = 0; i < smallCircles.length / 2; i++)
-    {
-        mainBody.addCircle(0.35 * PTM, smallCircles[2 * i + 0], smallCircles[2 * i + 1]);
-    }
-
-    for (var i = 0; i < mediumCircles.length / 2; i++)
-    {
-        mainBody.addCircle(1 * PTM, mediumCircles[2 * i + 0], mediumCircles[2 * i + 1]);
-    }
-
-    for (var i = 0; i < largeCircles.length / 2; i++)
-    {
-        mainBody.addCircle(2.8 * PTM, largeCircles[2 * i + 0], largeCircles[2 * i + 1]);
-    }
-    
-    // Add gutter fixture
-    gutterFixture = mainBody.addEdge(gutterVertices[0], gutterVertices[1], gutterVertices[2], gutterVertices[3]);
-    gutterFixture.SetSensor(true);
-    
-    // Set restitution for launcher
-    game.physics.box2d.restitution = 2; 
-    mainBody.addEdge(launcherVertices[0], launcherVertices[1], launcherVertices[2], launcherVertices[3]);
-    
-    // ball
-    game.physics.box2d.restitution = 0.1;
-    ballBody = new Phaser.Physics.Box2D.Body(this.game, null, ballStart[0] * (PTM / arrayMultiplier), ballStart[1] * (PTM / arrayMultiplier));
-    ballBody.setCircle(0.64 * PTM);
-    ballBody.setFixtureContactCallback(gutterFixture, onHitGutter, this);
-    ballBody.bullet = true;
-
-    // Flippers
-    game.physics.box2d.restitution = 0.1;
-
-    var leftFlipperBody = new Phaser.Physics.Box2D.Body(this.game, null, -8 * PTM, -7.99956 * PTM, 2);
-    leftFlipperBody.addPolygon(leftFlipperVertices);
-
-    var rightFlipperBody = new Phaser.Physics.Box2D.Body(this.game, null, 6.4 * PTM, -7.99956 * PTM, 2);
-    rightFlipperBody.addPolygon(rightFlipperVertices);
-    
-    // Flipper joints
-    var motorSpeed = 2;
-    var motorTorque = 100;  
-    // bodyA, bodyB, ax, ay, bx, by, motorSpeed, motorTorque, motorEnabled, lowerLimit, upperLimit, limitEnabled
-    flipperJoints[0] = game.physics.box2d.revoluteJoint(mainBody,  leftFlipperBody,  -8*PTM,-7.99956*PTM, 0,0, motorSpeed, motorTorque, true, -25, 25, true );
-    flipperJoints[1] = game.physics.box2d.revoluteJoint(mainBody, rightFlipperBody, 6.4*PTM,-7.99956*PTM, 0,0, motorSpeed, motorTorque, true, -25, 25, true );
-    
-    cursors = {
-        "sideButtonLeft": game.input.keyboard.addKey(Phaser.Keyboard.C), // 1SW7
-        "sideButoonRight": game.input.keyboard.addKey(Phaser.Keyboard.J), // 2SW7
-    };
-    
-    var caption = game.add.text(5, 5, 'Score: ', { fill: '#ffffff', font: '20pt Arial' });
-    caption.fixedToCamera = true;
-}
-
-function onHitGutter(body1, body2, fixture1, fixture2, begin) {
-    needReset = true; // this occurs inside the world Step, so don't do the actual reset here
-}
-
-function update() {
-    
-    if (needReset)
-    {
-        ballBody.x = ballStart[0]*PTM;
-        ballBody.y = ballStart[1]*PTM;
-        ballBody.velocity.x = 0;
-        ballBody.velocity.y = 0;
-        ballBody.angularVelocity = 0;
-        needReset = false;
-    }
-    
-    var flipperSpeed = 20; // rad/s
-    
-    if (cursors.sideButtonLeft.isDown)
-    {
-        flipperJoints[0].SetMotorSpeed(-flipperSpeed);
-    }
-    else
-    {
-        flipperJoints[0].SetMotorSpeed(flipperSpeed);
-    }
-    
-    if (cursors.sideButoonRight.isDown)
-    {
-        flipperJoints[1].SetMotorSpeed(flipperSpeed);
-    }
-    else
-    {
-        flipperJoints[1].SetMotorSpeed(-flipperSpeed);
-    }
-    
-}
-
-function render() {
-
-    game.debug.box2dWorld();
+const CONFIG = new Config()
+if (document.location.hash==='#keyboard') {
+    CONFIG.useKeyboard()
 
 }
+if (document.location.hash==='#demo') {
+    CONFIG.enableDemoMode()
+}
+
+function load() {
+    new PinballGame(CONFIG);
+}
+
+class PinballGame {
+    constructor(config) {
+        this.config = config
+        // engine (shared)
+        this.engine = Matter.Engine.create();
+        this.waitingForShooter = true;
+        // world (shared)
+        this.world = this.engine.world;
+        this.world.bounds = this.config.bounds
+        this.world.gravity.y = this.config.GRAVITY
+
+        // render (shared)
+        this.render = Matter.Render.create({
+            element: document.querySelector('.container'),
+            engine: this.engine,
+            sprite: {
+                texture: './img/background.svg'
+            },
+            options: {
+                width: this.world.bounds.max.x,
+                height: this.world.bounds.max.y,
+                wireframes: this.config.WIREFRAMES,
+                background: this.config.COLORS.BACKGROUND
+            }
+        });
+        Matter.Render.run(this.render);
+
+        Matter.Runner.run(Matter.Runner.create(), this.engine);
+        this.stopperGroup = Matter.Body.nextGroup(true);
+        this.currentScore = 0;
+        try {
+            this.highScore = localStorage.getItem('highScore');
+        } catch (SecurityError) {
+            this.highScore = 0;
+        }
+        this.maxVelocity = 0;
+        this.createStaticBodies();
+        this.createPaddles();
+        this.createPinball();
+        this.createEvents();
+        if (this.config.demoMode) {
+            this.launchPinball();
+        }
+    
+    }
+
+    createStaticBodies() {
+        Matter.World.add(this.world, new Boundary(this.world, 'top', this.config.COLORS.OUTER).boundary)
+        Matter.World.add(this.world, new Boundary(this.world, 'bottom', this.config.COLORS.OUTER).boundary)
+        Matter.World.add(this.world, new Boundary(this.world, 'left', this.config.COLORS.OUTER).boundary)
+        Matter.World.add(this.world, new Boundary(this.world, 'right', this.config.COLORS.OUTER).boundary)
+
+        this.config.staticBodies.forEach((item) => Matter.World.add(this.world, item))       
+        Matter.World.add(this.world, [
+            this.reset(225, 50),
+        ]);
+    }
+
+    createPaddles() {
+
+        // these bodies keep paddle swings contained, but allow the ball to pass through
+        let leftUpStopper = new Stopper(160, this.world.bounds.max.y-209, 'up', 'paddle-left-comp', this.stopperGroup);
+        let leftDownStopper = new Stopper(140, this.world.bounds.max.y-57, 'down', 'paddle-left-comp', this.stopperGroup);
+        let rightUpStopper = new Stopper(290, this.world.bounds.max.y-209, 'up', 'paddle-right-comp', this.stopperGroup);
+        let rightDownStopper = new Stopper(310, this.world.bounds.max.y-57, 'down', 'paddle-right-comp', this.stopperGroup);
+        Matter.World.add(this.world, [leftUpStopper.stopper, leftDownStopper.stopper, rightUpStopper.stopper, rightDownStopper.stopper]);
+
+        // this group lets paddle pieces overlap each other
+        let paddleGroup = Matter.Body.nextGroup(true);
+        this.paddleLeft = new Paddle(this.world, paddleGroup, 'left', 170)
+        this.paddleRight = new Paddle(this.world, paddleGroup, 'right', 280)
+        leftUpStopper.paddle = this.paddleLeft
+        leftDownStopper.paddle = this.paddleLeft
+        rightUpStopper.paddle = this.paddleRight
+        rightDownStopper.paddle = this.paddleRight
+    }
+
+    createPinball() {
+        this.pinball = Matter.Bodies.circle(0, 0, 14, {
+            label: 'pinball',
+            collisionFilter: {
+                group: this.stopperGroup
+            },
+            render: {
+                fillStyle: this.config.COLORS.PINBALL
+            }
+        });
+        Matter.World.add(this.world, this.pinball);
+        this.launcher = new Launcher(this.pinball)
+
+        this.resetPinball();
+
+    }
+
+    createEvents() {
+        // events for when the pinball hits stuff
+        Matter.Events.on(this.engine, 'collisionStart', (event) => {
+            event.pairs.forEach((pair) => {
+                if (pair.bodyB.label === 'pinball') {
+                    switch (pair.bodyA.label) {
+                        case 'reset':
+                            this.resetPinball();
+                            break;
+                        case 'bumper':
+                            this.pingBumper(pair.bodyA);
+                            break;
+                    }
+                    if (this.config.demoMode) {
+                        switch (pair.bodyA.label) {
+                            case 'paddle-left-tip':
+                                this.paddleLeft.up()
+                                setTimeout(() => { this.paddleLeft.down()}, 250);
+                                break;
+                            case 'paddle-right-tip':
+                                this.paddleRight.up();
+                                setTimeout(() => { this.paddleRight.down()}, 250);
+                                break;
+                            case 'reset':
+                                this.launchPinball()
+                                break;
+                        }
+                    }
+                }
+            });
+        });
+
+        // regulate pinball
+        Matter.Events.on(this.engine, 'beforeUpdate', (event) => {
+            // bumpers can quickly multiply velocity, so keep that in check
+            Matter.Body.setVelocity(this.pinball, {
+                x: Math.max(Math.min(this.pinball.velocity.x, this.config.MAX_VELOCITY), -this.config.MAX_VELOCITY),
+                y: Math.max(Math.min(this.pinball.velocity.y, this.config.MAX_VELOCITY), -this.config.MAX_VELOCITY),
+            });
+
+            // cheap way to keep ball from going back down the shooter lane
+            if (!this.waitingForShooter && this.pinball.position.x > 450 && this.pinball.velocity.y > 0) {
+                Matter.Body.setVelocity(this.pinball, { x: 0, y: -10 });
+            }
+        });
+
+        // mouse drag (god mode for grabbing pinball)
+        Matter.World.add(this.world, Matter.MouseConstraint.create(this.engine, {
+            mouse: Matter.Mouse.create(this.render.canvas),
+            constraint: {
+                stiffness: 0.2,
+                render: {
+                    visible: false
+                }
+            }
+        }));
+
+        // keyboard paddle events
+        $('body').on('keydown', (e) => {
+            if (e.key === this.config.keys.leftPaddle) { // left arrow key
+                this.paddleLeft.up()
+                e.stopPropagation()
+            } else if (e.key === this.config.keys.rightPaddle) { // right arrow key
+                this.paddleRight.up()
+                e.stopPropagation()
+            } else if (this.waitingForShooter) {
+                if (e.key === this.config.keys.shooter) {
+                    this.launcher.startPulling()                    
+                    e.stopPropagation()
+                }
+            }
+        });
+        $('body').on('keyup', (e) => {
+            if (e.key === this.config.keys.leftPaddle) { // left arrow key
+                this.paddleLeft.down()
+                e.stopPropagation()
+            } else if (e.key === this.config.keys.rightPaddle) { // right arrow key
+                this.paddleRight.down()
+                e.stopPropagation()
+            } else if (this.waitingForShooter) {
+                if  (e.key ===this.config.keys.shooter) {
+                    this.launchPinball()
+                    e.stopPropagation()
+                }
+            }
+        });
+    }
+
+    resetPinball() {
+        this.waitingForShooter = true
+        this.updateScore(0);
+        Matter.Body.setPosition(this.pinball, { x: 465, y: 957 });
+    }
+
+    launchPinball() {
+        if (this.waitingForShooter) {
+            this.launcher.launch()
+            this.waitingForShooter = false;
+        }
+    }
+
+    pingBumper(bumper) {
+        this.updateScore(this.currentScore + 10);
+
+        // flash color
+        bumper.render.strokeStyle = this.config.COLORS.BUMPER_LIT;
+        bumper.render.lineWidth = 12;
+        setTimeout(() => {
+            bumper.render.strokeStyle = this.config.COLORS.BUMPER;
+            bumper.render.lineWidth = 0;
+        }, 100);
+    }
+
+    get highScore() {
+        return this._highScore
+    }
+
+    set highScore(newHighScore) {
+        this._highScore = newHighScore
+        localStorage.setItem('highScore', this._highScore)
+        $highScore.text(this._highScore)
+    }
+
+    updateScore(newCurrentScore) {
+        this.currentScore = newCurrentScore;
+        $currentScore.text(this.currentScore);
+        if (this.currentScore > this.highScore) {
+            this.highScore = this.currentScore;
+        }
+    }
+
+    // matter.js has a built in random range function, but it is deterministic
+    rand(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+
+    // contact with these bodies causes pinball to be relaunched
+    reset(x, width) {
+        return Matter.Bodies.rectangle(x, this.world.bounds.max.y - 19, width, 2, {
+            label: 'reset',
+            isStatic: true,
+            render: {
+                fillStyle: '#fff'
+            }
+        });
+    }    
+}
+
+window.addEventListener('load', load, false);
